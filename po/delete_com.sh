2cfb5cf1fcbb9890c4d5256a5b28bbdae4469cee
Jan Macku <jamacku@redhat.com>
2019-03-08 15:18:36 +0100

    Add script for delete unneccesary blocks in *.po

---
+++ po/delete_com.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+## Delete blocks in *.po files for source code, that is no longer present
+
+for file in *.po;
+do
+  awk -i '/^.*(#: ..\/chkconfig.c:|#: ..\/ntsysv.c:|#: ..\/leveldb.c:).*$/{while(getline && $0 != ""){}}1' "${file}" >> "${file}".tmp
+  cat -s "${file}".tmp >> "${file}"
+done
