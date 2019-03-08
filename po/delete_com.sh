b1d44deec97117fc3273ddb3732f41209e8b1e56
Jan Macku <jamacku@redhat.com>
2019-03-08 15:35:43 +0100

    Remove unnecessary blocs from *.po files

--- po/delete_com.sh
+++ po/delete_com.sh
@@ -2,8 +2,9 @@
 
 ## Delete blocks in *.po files for source code, that is no longer present
 
-for file in *.po;
+for file in *.po*;
 do
-  awk -i '/^.*(#: ..\/chkconfig.c:|#: ..\/ntsysv.c:|#: ..\/leveldb.c:).*$/{while(getline && $0 != ""){}}1' "${file}" >> "${file}".tmp
-  cat -s "${file}".tmp >> "${file}"
+  awk '/^.*(#: ..\/chkconfig.c:|#: ..\/ntsysv.c:|#: ..\/leveldb.c:).*$/{while(getline && $0 != ""){}}1' "${file}" > "${file}".tmp
+  cat -s "${file}".tmp > "${file}"
+  rm "${file}".tmp
 done
